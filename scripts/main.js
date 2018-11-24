5718331fa09705c293803ed39e0027c9822255cf
David Khamitsev <davidhamicev@mail.ru>
2018-11-24 13:01:28 +0300

    Update 2018-11-24T10:00:53.800Z

--- scripts/main.js
+++ scripts/main.js
@@ -16,7 +16,6 @@ function mobileMenu() {
 		navMobile.classList.toggle('is-active');
 	};
 };
-
 mobileMenu();
 
 //Слайдер 
@@ -71,24 +70,87 @@ function slider() {
 		}, 50);
 	};
 };
-
 slider();
 
 
-//всплывающее меню 
+//всплывающее меню слайдера
 function showConsist() {
-	const consist = document.querySelectorAll('.burger__note--consist');
-	const composition = document.querySelector('.burger__composition');
+	const consists = document.querySelectorAll('.burger__note--consist'); //блочок с бургером
+	const compositions = document.querySelectorAll('.burger__composition'); //всплывающий блок ингредиентов
 
-	for (const iterator of consist) {
-		iterator.addEventListener('mouseover', toggleClass);
-		iterator.addEventListener('mouseout', toggleClass);
+	for (const consistItem of consists) {
+		consistItem.addEventListener('mouseover', hiddenMenu);
+		consistItem.addEventListener('mouseout', hiddenMenu);
 	}
 
-	function toggleClass(e) {
+	function hiddenMenu(e) {
 		e.preventDefault();
-		composition.classList.toggle('onVisible');
+		for (const item of compositions) {
+			item.classList.toggle('onVisible');
+		};
 	};
 };
-
 showConsist();
+
+//горизонтальный аккордеон
+function teamAccordion() {
+	const accordionItems = document.querySelectorAll('.team-accordeon__item');
+	const teamAcc = document.querySelector('.team-accordeon');
+
+	teamAcc.addEventListener('click', event => {
+
+		const target = event.target;
+
+		if (target.classList.contains('team-accordeon__head')) {
+			const teamItem = target.parentNode;
+			const itemContent = target.nextElementSibling;
+			const contentHeight = itemContent.firstElementChild.clientHeight;
+
+			for (const item of accordionItems) {
+				if (item !== teamItem) {
+					item.classList.remove('is-active');
+					item.lastElementChild.style.height = 0;
+				}
+			}
+			if (teamItem.classList.contains('is-active')) {
+				teamItem.classList.remove('is-active');
+				itemContent.style.height = 0;
+			} else {
+				teamItem.classList.add('is-active');
+				itemContent.style.height = contentHeight + 'px';
+			}
+		}
+	});
+};
+teamAccordion();
+
+//вертикальный аккордион меню 
+function menuAccordion() {
+	const menuItem = document.querySelectorAll('.menu-accordeon__item');
+	const menuAcc = document.querySelector('.menu-accordeon__list');
+
+	menuAcc.addEventListener('click', event => {
+		event.preventDefault();
+		const target = event.target;
+		
+		for (const it of menuItem) {
+			if ( it !== target.parentNode) {
+				it.classList.remove('is-active');
+				it.lastElementChild.style.width = 0;
+			}
+		}
+		if (target.classList.contains('menu-accordeon__link')) {
+			const menuContent = target.nextElementSibling;
+			//console.log(menuContent.parentNode);
+		
+			if (menuContent.parentNode.classList.contains('is-active')) {
+				menuContent.parentNode.classList.remove('is-active');
+				menuContent.style.width = 0;
+			} else {
+				menuContent.parentNode.classList.add('is-active');
+				menuContent.style.width = 520 + 'px';
+			}
+		}
+	});
+};
+menuAccordion();
